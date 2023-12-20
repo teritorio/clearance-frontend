5064df530b00caff63c1e03c51128fbdbd5bb891
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-12-20 17:21:08 +0100

    feat: style loggin button

--- components/User.vue
+++ components/User.vue
@@ -1,13 +1,13 @@
 <template>
-  <form v-if="!user" :action="loginUrl" method="post">
-    <button type="submit">{{ $t('user.login') }}</button>
+  <form v-if="!user" ref="form" :action="loginUrl" method="post">
+    <el-button type="primary" @click="submit">{{ $t('user.login') }}</el-button>
   </form>
   <template v-else>
     <el-avatar :src="user.osm_image_url">
       {{ user.osm_name }}
     </el-avatar>
     <span>{{ user.osm_name }}</span>
-    <button @click="logout">{{ $t('user.logout') }}</button>
+    <el-button @click="logout">{{ $t('user.logout') }}</el-button>
   </template>
 </template>
 
@@ -25,6 +25,11 @@ export default defineNuxtComponent({
     },
   },
 
+  setup() {
+    const form = shallowRef(null)
+    return { form }
+  },
+
   computed: {
     loginUrl(): string {
       return useRuntimeConfig().public.API + '../../../users/auth/osm_oauth2'
@@ -32,6 +37,9 @@ export default defineNuxtComponent({
   },
 
   methods: {
+    submit(): void {
+      this.form.submit()
+    },
     logout(): void {
       userLogout(useRuntimeConfig().public.API)
     },
