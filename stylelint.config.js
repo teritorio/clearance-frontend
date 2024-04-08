76b9e0968810a6f276b2783d489f9275a76ace4b
Noé Viricel <noe.viricel@wazo-lab.io>
2024-04-08 18:04:54 +0200

    chore: remove prettier in favor of eslint stylistic #12

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,7 +3,6 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-recommended-vue',
-    'stylelint-config-prettier',
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
