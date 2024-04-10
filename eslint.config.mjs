5d42f894bb636d39550e712981d4ad3268789332
Noé Viricel <noe.viricel@wazo-lab.io>
2024-04-10 18:06:10 +0200

    chore: manual lint fix #20

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,10 @@
 import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(antfu({}))
+export default withNuxt({
+  files: ['**/*.{js,ts,vue,json,yml,markdown,mjs}', '.vscode'],
+}, antfu({
+  rules: {
+    'vue/no-unused-refs': 0,
+  },
+}))
