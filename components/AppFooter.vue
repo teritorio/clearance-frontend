29f9ecebab43fc42b5f6d7408c6460dff6198578
Noé Viricel <noe.viricel@wazo-lab.io>
2024-04-18 18:01:08 +0200

    chore: create app-footer component #10

---
+++ components/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer>
+    <p>
+      {{ $t('app.attribution.data') }}
+      <a href="https://www.openstreetmap.org/copyright" target="_blank">{{
+        $t('app.attribution.osm')
+      }}</a>
+    </p>
+  </footer>
+</template>
