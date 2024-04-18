8511342b6f622961bf670d42f489258048621d8b
Noé Viricel <noe.viricel@wazo-lab.io>
2024-04-18 19:59:01 +0200

    chore: revamp app-menu layout #42

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,10 +1,10 @@
 <template>
-  <footer>
+  <el-footer>
     <p>
       {{ $t('app.attribution.data') }}
       <a href="https://www.openstreetmap.org/copyright" target="_blank">{{
         $t('app.attribution.osm')
       }}</a>
     </p>
-  </footer>
+  </el-footer>
 </template>
--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import type { User } from '~/libs/types'
 
-const back = ref(true)
 const user = useState<User>('user')
+
 const { locale, locales, setLocale } = useI18n()
 const availableLocales = computed(() => {
   return locales.value.filter((i) => i.code !== locale.value)
 })
+
 // Function from https://dev.to/jorik/country-code-to-flag-emoji-a21
 function getFlagEmoji(countryCode: string) {
   const codePoints = countryCode
@@ -18,21 +19,11 @@ function getFlagEmoji(countryCode: string) {
 </script>
 
 <template>
-  <el-menu mode="horizontal" :ellipsis="false">
-    <el-menu-item v-if="back" index="0">
-      <nuxt-link to="/" :title="$t('app.back')">
-        <img src="/favicon.svg" style="width: 6em" />
-      </nuxt-link>
-    </el-menu-item>
-    <el-menu-item v-else index="0">
-      <img src="/favicon.svg" style="width: 6em" />
-    </el-menu-item>
-    <div class="flex-grow" />
-    <el-menu-item index="1">
-      <slot name="header" />
-    </el-menu-item>
-    <div class="flex-grow" />
-    <el-menu-item index="2">
+  <el-header height="68px">
+    <nuxt-link to="/" :title="$t('app.back')">
+      <img src="/favicon.svg" />
+    </nuxt-link>
+    <div>
       <el-select
         v-model="locale"
         class="m-2"
@@ -48,15 +39,30 @@ function getFlagEmoji(countryCode: string) {
           :fit-input-width="true"
         />
       </el-select>
-    </el-menu-item>
-    <el-menu-item index="3">
       <user-profile :user="user" />
-    </el-menu-item>
-  </el-menu>
+    </div>
+  </el-header>
 </template>
 
 <style scoped>
-.flex-grow {
-  flex-grow: 1;
+header {
+  align-items: center;
+  box-shadow: var(--el-box-shadow-light);
+  display: flex;
+}
+
+header > div {
+  align-items: center;
+  display: flex;
+  gap: 8px;
+  margin-left: auto;
+}
+
+.el-select {
+  width: 100px;
+}
+
+img {
+  width: 68px;
 }
 </style>
