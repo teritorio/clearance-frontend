a54ec5afc239f837f782618fbd75c362d0cf204f
Noé Viricel <noe.viricel@wazo-lab.io>
2024-04-18 18:01:08 +0200

    chore: create app-footer component #10

--- app.vue
+++ app.vue
@@ -77,13 +77,6 @@ function getFlagEmoji(countryCode: string) {
       </el-menu-item>
     </el-menu>
     <nuxt-page />
-    <footer>
-      <p>
-        {{ $t('app.attribution.data') }}
-        <a href="https://www.openstreetmap.org/copyright" target="_blank">{{
-          $t('app.attribution.osm')
-        }}</a>
-      </p>
-    </footer>
+    <app-footer />
   </nuxt-layout>
 </template>
