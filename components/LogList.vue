83372fb64f5df926bc792d725ca9727f0c8e7657
Noé Viricel <noe.viricel@wazo-lab.io>
2024-04-24 14:43:15 +0200

    chore: change project prop to projectSlug #72

--- components/LogList.vue
+++ components/LogList.vue
@@ -2,7 +2,7 @@
 import type { Log, ObjectId, User } from '~/libs/types'
 
 const props = defineProps<{
-  project: string
+  projectSlug: string
   logs: Log[]
 }>()
 
@@ -10,13 +10,12 @@ defineEmits<{
   (e: 'accept', objectIds: ObjectId[]): void
 }>()
 
-const scrollCount = ref<number>(10)
-
 const user = useState<User>('user')
 const isProjectUser = computed(() => {
-  return !!user.value?.projects?.includes(props.project)
+  return !!user.value?.projects?.includes(props.projectSlug)
 })
 
+const scrollCount = ref<number>(10)
 function scrollLoad() {
   scrollCount.value += 10
 }
@@ -29,7 +28,7 @@ function scrollLoad() {
         v-for="log in (logs || []).slice(0, scrollCount + 1)"
         :key="log.id"
         :log="log"
-        :project="project"
+        :project="projectSlug"
         :project-user="isProjectUser"
         @accept="$emit('accept', [$event])"
       />
@@ -38,10 +37,3 @@ function scrollLoad() {
     <iframe name="hidden_josm_target" style="display: none" />
   </div>
 </template>
-
-<style scoped>
-.item {
-  margin-top: 0.7em;
-  margin-right: 1.3em;
-}
-</style>
