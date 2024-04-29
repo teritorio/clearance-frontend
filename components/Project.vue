0d99b456890bfa7865a9348d4a7f22f2a716488d
Noé Viricel <noe.viricel@wazo-lab.io>
2024-04-29 15:05:35 +0200

    chore: use project-light within project component #85

--- components/Project.vue
+++ components/Project.vue
@@ -48,29 +48,7 @@ export default defineNuxtComponent({
 <template>
   <el-card>
     <template #header>
-      <div class="card-header">
-        <span>
-          <span class="title">{{ $i18nHash(project.title) }}</span>
-          <el-tag
-            v-for="tag in project.project_tags"
-            :key="tag"
-            class="item"
-            size="small"
-          >{{ tag }}</el-tag>
-          <br />
-          <el-text class="mx-1" type="info">
-            {{ $i18nHash(project.description) }}
-          </el-text>
-        </span>
-        <el-button-group>
-          <nuxt-link class="el-button" :to="`/${project.id}/validators/`">
-            {{ $t('project.settings') }}
-          </nuxt-link>
-          <nuxt-link class="el-button el-button--primary" :to="`/${project.id}/changes_logs/`">
-            {{ $t('project.details') }}
-          </nuxt-link>
-        </el-button-group>
-      </div>
+      <project-light :project="project" />
     </template>
     <div>
       <el-row id="stats">
@@ -159,21 +137,5 @@ export default defineNuxtComponent({
   font-weight: var(--el-statistic-content-font-weight);
   font-size: var(--el-statistic-content-font-size);
   color: var(--el-statistic-content-color);
-}
-
-:deep(.card-header) {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.title {
-  font-weight: 600;
-  padding-right: 1em;
-}
-
-.item {
-  margin-top: 0.7em;
-  margin-right: 1.3em;
 }
 </style>
