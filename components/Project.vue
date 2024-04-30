19ae508132f0d70c4c7d6d9de8aeb083174f6135
Noé Viricel <noe.viricel@wazo-lab.io>
2024-04-30 12:05:46 +0200

    feat: add user-groups to accordion #85

--- components/Project.vue
+++ components/Project.vue
@@ -11,6 +11,14 @@ export default defineNuxtComponent({
     },
   },
 
+  setup() {
+    const activeName = ref('')
+
+    return {
+      activeName,
+    }
+  },
+
   computed: {
     apiUrl(): string {
       return useRuntimeConfig().public.API
@@ -27,42 +35,47 @@ export default defineNuxtComponent({
     <template #header>
       <project-light :project="project" title-tag="h3" />
     </template>
-    <div>
-      <el-row>
+    <el-collapse v-model="activeName" accordion>
+      <el-collapse-item :title="$t('project.seeMore')" name="1">
         <UserGroups :user-groups="Object.values(project.user_groups)" />
-      </el-row>
-      <el-row>
-        <ul>
-          <li>
-            {{ $t('project.overpassUrl') }}
-            <a :href="overpassUrl">{{ overpassUrl }}</a>
-          </li>
-          <li>
-            {{ $t('project.extract') }}
-            <a :href="`${apiUrl}/${project.id}/extract/${project.id}.osm.bz2`">
-              {{ `${project.id}.osm.bz2` }}
-            </a>
-          </li>
-          <li>
-            <a :href="`${apiUrl}/${project.id}/extract/update/`">{{
-              $t('project.diff')
-            }}</a>
-          </li>
-        </ul>
-      </el-row>
-    </div>
-    <p>
-      <span>{{ $t('app.project.mainContacts') }}</span>
-      <span v-for="user in project.main_contacts" :key="user">
-        <a :href="`https://www.openstreetmap.org/user/${user}`" target="_blank">👤&nbsp;{{ user }}</a>
-      </span>
-    </p>
-    <p>{{ $t('app.project.join') }}</p>
+        <el-row>
+          <ul>
+            <li>
+              {{ $t('project.overpassUrl') }}
+              <a :href="overpassUrl">{{ overpassUrl }}</a>
+            </li>
+            <li>
+              {{ $t('project.extract') }}
+              <a :href="`${apiUrl}/${project.id}/extract/${project.id}.osm.bz2`">
+                {{ `${project.id}.osm.bz2` }}
+              </a>
+            </li>
+            <li>
+              <a :href="`${apiUrl}/${project.id}/extract/update/`">{{
+                $t('project.diff')
+              }}</a>
+            </li>
+          </ul>
+        </el-row>
+        <p>
+          <span>{{ $t('app.project.mainContacts') }}</span>
+          <span v-for="user in project.main_contacts" :key="user">
+            <a :href="`https://www.openstreetmap.org/user/${user}`" target="_blank">👤&nbsp;{{ user }}</a>
+          </span>
+        </p>
+        <p>{{ $t('app.project.join') }}</p>
+      </el-collapse-item>
+    </el-collapse>
   </el-card>
 </template>
 
 <style scoped>
 :deep(.el-card__header) {
   background-color: #fafafa;
+}
+
+ul, p {
+  font-size: initial;
+  line-height: initial;
 }
 </style>
