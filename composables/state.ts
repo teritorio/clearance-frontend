abcb5cb701b70718d9a612ce9838832d115f5ddf
Noé Viricel <noe.viricel@wazo-lab.io>
2024-05-06 15:35:42 +0200

    chore: apply shared state to projects #110

--- composables/state.ts
+++ composables/state.ts
@@ -1,3 +1,5 @@
-import type { Log } from '~/libs/types'
+import type { Log, Project } from '~/libs/types'
 
 export const useLogs = () => useState<Log[]>('logs')
+
+export const useProjects = () => useState<Project[]>('projects')
