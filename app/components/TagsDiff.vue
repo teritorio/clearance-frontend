8861141e179526fd32b1a880d115229f38d56a87
Noé Viricel <noe.viricel@wazo-lab.io>
2026-04-20 17:41:43 +0200

    fix: restore action dropdown with badge in TagsDiff
    Bring back the el-dropdown + el-badge pattern from the original Diff.vue
    component. Each action tag now shows a badge with the number of options
    and a dropdown revealing validation rule details on hover.
    
    Closes #374

--- app/components/TagsDiff.vue
+++ app/components/TagsDiff.vue
@@ -146,16 +146,50 @@ function diffText(before: string, after: string): Change[] {
                   ?
                 </el-tag>
                 <template v-else>
-                  <el-tag
+                  <el-dropdown
                     v-for="(action, actionIndex) in actions"
                     :key="actionIndex"
-                    :type="action[1] === 'reject' ? 'danger' : 'info'"
-                    size="small"
-                    :disable-transitions="true"
+                    :show-timeout="0"
                     class="action-tag"
                   >
-                    {{ action[0] }}
-                  </el-tag>
+                    <span class="el-dropdown-link">
+                      <el-badge
+                        :value="
+                          (action && action[2] && Object.keys(action[2]).length)
+                            || undefined
+                        "
+                        :type="action[1] === 'reject' ? 'danger' : 'info'"
+                      >
+                        <el-tag
+                          :type="action[1] === 'reject' ? 'danger' : 'info'"
+                          size="small"
+                          :disable-transitions="true"
+                        >
+                          {{ action[0] }}
+                          <template v-if="action && action[2]">
+                            ⮟
+                          </template>
+                        </el-tag>
+                      </el-badge>
+                    </span>
+                    <template v-if="action && action[2]" #dropdown>
+                      <el-dropdown-menu v-for="(option, i) in action[2]" :key="i">
+                        <el-dropdown-item>
+                          {{ i }}
+                          <template v-if="Array.isArray(option)">
+                            <ul>
+                              <li v-for="op in option" :key="op">
+                                {{ op }}
+                              </li>
+                            </ul>
+                          </template>
+                          <template v-else>
+                            {{ option }}
+                          </template>
+                        </el-dropdown-item>
+                      </el-dropdown-menu>
+                    </template>
+                  </el-dropdown>
                 </template>
               </th>
             </tr>
