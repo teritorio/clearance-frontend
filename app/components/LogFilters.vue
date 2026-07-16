46c1d80bc85b6db467d60eaaf7aa8936339ef7cb
Noé Viricel <noe.viricel@wazo-lab.io>
2026-07-17 06:05:54 +0700

    style: filtres amber uniforme, agrandir icône settings

--- app/components/LogFilters.vue
+++ app/components/LogFilters.vue
@@ -103,7 +103,7 @@ const hasActiveFilters = computed(() => Object.keys(filters.value ?? {}).length 
           :max="999"
         >
           <el-button
-            type="danger"
+            type="primary"
             :plain="filters?.filterByAction !== key"
             :disabled="!!(filters?.filterByAction && filters?.filterByAction !== key)"
             size="small"
@@ -149,7 +149,7 @@ const hasActiveFilters = computed(() => Object.keys(filters.value ?? {}).length 
           :max="999"
         >
           <el-button
-            type="warning"
+            type="primary"
             :plain="filters?.filterBySelectors !== match.selectors.join()"
             :disabled="!!(filters?.filterBySelectors && filters?.filterBySelectors !== match.selectors.join())"
             size="small"
@@ -172,7 +172,7 @@ const hasActiveFilters = computed(() => Object.keys(filters.value ?? {}).length 
           :max="999"
         >
           <el-button
-            type="info"
+            type="primary"
             :plain="filters?.filterByUsers !== key"
             :disabled="!!(filters?.filterByUsers && filters?.filterByUsers !== key)"
             size="small"
