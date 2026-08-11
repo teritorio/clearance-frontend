05dad55e7bc4ad2e69cf4a214aae8936651ec20e
Noé Viricel <noe.viricel@wazo-lab.io>
2026-08-12 01:00:27 +0700

    fix: use var(--el-color-danger) for option count badge

--- app/components/LogFilters.vue
+++ app/components/LogFilters.vue
@@ -256,7 +256,7 @@ const hasActiveFilters = computed(() => Object.keys(filters.value ?? {}).length 
   flex-shrink: 0;
   font-size: 0.75rem;
   font-weight: 700;
-  color: var(--el-color-danger-dark-2);
+  color: var(--el-color-danger);
   background: var(--el-color-danger-light-9);
   border-radius: 10px;
   padding: 1px 7px;
